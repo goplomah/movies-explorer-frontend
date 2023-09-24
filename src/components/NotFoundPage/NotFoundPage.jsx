adb15b776caabb14b360da3b44d32caa594c270d
Maks Subochev <maxim.subochev@yandex.ru>
2023-09-24 19:25:47 +0300

    feat: логика кнопки на стр 404

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,15 @@
 import "./NotFoundPage.css";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
+  const navigation = useNavigate();
+  const goToBack = () => navigation(-1);
+
   return (
     <section className='not-found-page'>
       <h2 className="not-found-page__title">404</h2>
       <p className="not-found-page__text">Страница не найдена</p>
-      <button className="not-found-page__button opacity" type="button">Назад</button>
+      <button className="not-found-page__button opacity" type="button" onClick={goToBack}>Назад</button>
     </section>
   );
 }
