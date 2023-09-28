42a72df08ebd207d9e37e09141abcb021e82222c
Maks Subochev <maxim.subochev@yandex.ru>
2023-09-28 21:46:52 +0300

    feat: адаптив лэндинга для 768px

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -7,10 +7,12 @@ function AboutMe() {
       <h2 className="about-me__title">Студент</h2>
       <div className="about-me__wrapper">
         <div className="about-me__text-block">
+          <div className="about-me__wrapper-text">
           <h3 className="about-me__name">Кот Виталий</h3>
           <p className="about-me__description">Фронтенд-разработчик, 32 годика</p>
           <p className="about-me__text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
 и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+          </div>
           <a target="_blank" href="https://github.com/goplomah" className="about-me__link opacity" rel="noreferrer">Github</a>
         </div>
         <img src={cat} alt="аватар кота" className="about-me__photo" />
